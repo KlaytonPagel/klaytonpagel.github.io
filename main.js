c2b210df2535e1236dcc47ca7fe9eda41da351c4
Klayton Pagel <pagelk@lopers.unk.edu>
2024-01-05 15:25:04 -0600

    added main page
    added a main page that you can go to different sites from

---
+++ main.js
@@ -0,0 +1,5 @@
+let vorpalButton = document.querySelector('#vorpalButton')
+let testButton = document.querySelector('#testButton')
+
+vorpalButton.onclick = function () {window.open('../vorpal_dive/index.html', '_self');}
+testButton.onclick = function () {window.open('../test_page/index.html', '_self');}
