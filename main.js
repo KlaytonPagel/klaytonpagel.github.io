c8538e5ef7001ec0103d4b28109bbdfa4992391b
Klayton Pagel <147002610+KlaytonPagel@users.noreply.github.com>
2024-02-03 12:18:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ let testButton = document.querySelector('#testButton')
 
 vorpalButton.onclick = function () {window.open('../vorpal_dive/index.html', '_self');}
 testButton.onclick = function () {window.open('../test_page/index.html', '_self');}
+sandButton.onclick = function () {window.open('../Simulations/index.html', '_self');}
