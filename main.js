07032ce1a5e4f2c8d3d386df7b1d06366e7d2178
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-05 00:44:08 -0600

    more bug fixes

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ let circleButton = document.querySelector('#circleButton')
 
 vorpalButton.onclick = function () {window.open('../vorpal_dive/index.html', '_self');}
 testButton.onclick = function () {window.open('../test_page/index.html', '_self');}
-sandButton.onclick = function () {window.open('../Simulations/Simulations/Sand Simulation/index.html', '_self');}
-circleButton.onclick = function () {window.open('../Simulations/Simulations/Circle Thing/index.html', '_self');}
+sandButton.onclick = function () {window.open('../Simulations/Sand_Simulation/index.html', '_self');}
+circleButton.onclick = function () {window.open('../Simulations/Circle_Thing/index.html', '_self');}
