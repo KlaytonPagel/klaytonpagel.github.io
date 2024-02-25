2130b8061a2baf3c5988e0c98ce00dca0681e1c4
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-25 09:39:23 -0600

    Update Bookmarklet

--- extensions/Bookmarklet/bookmarklet.js
+++ extensions/Bookmarklet/bookmarklet.js
@@ -1,9 +1,20 @@
 (function (){
-    let paragraphs = document.getElementsByClassName('main')
-    for (let index = 0; index < paragraphs.length; index++){
-        paragraphs[index].style.backgroundColor = "#424242";
-        paragraphs[index].style.width = "100px";
-        paragraphs[index].style.height = "100px";
-        alert('changed')
+    // Set variables for all elements you want to change
+    const divs = document.getElementsByTagName('div');
+    const ps = document.getElementsByTagName('p');
+    const spans = document.getElementsByTagName('spans');
+    const as = document.getElementsByTagName('a');
+
+    // Store all elements in an array
+    const elements = [divs, ps, spans, as];
+
+    // Loops through them changing values
+    for (let item = 0; item < elements.length; item++) {
+        let element = elements[item];
+        for (let index = 0; index < element.length; index++) {
+            element[index].style.backgroundColor ='#424242';
+            element[index].style.color = '#d0d0d0';
+        }
     }
+    alert('changed');
 })();
