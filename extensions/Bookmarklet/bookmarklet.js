a240563265176e81926781993e6c823e577faf44
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-26 07:54:17 -0600

    Updated Bookmarklet

--- extensions/Bookmarklet/bookmarklet.js
+++ extensions/Bookmarklet/bookmarklet.js
@@ -4,11 +4,12 @@
     const ps = document.getElementsByTagName('p');
     const spans = document.getElementsByTagName('span');
     const as = document.getElementsByTagName('a');
+    const iframe = document.getElementsByTagName('iframe');
     const x_tab_inner = document.getElementsByClassName('x-tab-inner');
     const x_tab_inner_default = document.getElementsByClassName('x-tab-inner-default');
 
     // Store all elements in an array
-    const elements = [divs, ps, spans, as, x_tab_inner, x_tab_inner_default];
+    const elements = [divs, ps, spans, as, x_tab_inner, x_tab_inner_default, iframe];
 
     // Loops through them changing values
     for (let item = 0; item < elements.length; item++) {
